bfd1ce75fc7de30757a1899efbc6ec272847a10c
Kiran Prajapati <officeofkiran@gmail.com>
2019-05-28 17:32:37 +0530

    Correct single quote for Côte d'Ivoire country

--- wp-country-database.php
+++ wp-country-database.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WP_Country_Database' ) ) {
 
 
 		public $version = "1.0.2";
-		public $db_version = "1.0.0";
+		public $db_version = "1.0.1";
 		public $db_version_current;
 		public $db_table;
 		private static $instance = null;
